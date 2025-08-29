6456e9976c0987f75acc4b227509d450afdb05a2
bpastorelli <brunopastorelli@hotmail.com>
2025-08-29 17:08:53 -0300

    Ajuste

--- src/main/java/br/com/vinculos/security/config/WebSecurity.java
+++ src/main/java/br/com/vinculos/security/config/WebSecurity.java
@@ -31,7 +31,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(requests -> requests
-                //.mvcMatchers(HttpMethod.GET, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
+                .mvcMatchers(HttpMethod.GET, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
+                .mvcMatchers(HttpMethod.POST, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
+                .mvcMatchers(HttpMethod.PUT, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
                 .mvcMatchers(HttpMethod.GET, "**/sgc/vinculo/**").permitAll()
                 .mvcMatchers(HttpMethod.POST, "**/sgc/vinculo/**").permitAll()
                 .mvcMatchers(HttpMethod.PUT, "**/sgc/vinculo/**").permitAll()
